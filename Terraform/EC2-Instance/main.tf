6af17fa04c0b7aa67bc9a73777abe34806288d30
Chittiboina Siva Narasimhulu <164983163+sivanarasimhulu21@users.noreply.github.com>
2026-03-14 20:08:58 +0530

    Add Terraform configuration for EC2 instance

---
+++ Terraform/EC2-Instance/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+resource "aws_instance" "my_ec2" {
+
+  ami           = "ami-0f5ee92e2d63afc18"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+
+}
